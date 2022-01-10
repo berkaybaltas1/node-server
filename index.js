25aafbb274b472808f21fcd547604880be6d3d56
Berkay Baltas <berkay.baltas@sourcedigital.net>
2022-01-09 22:19:24 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+const PORT = 3000
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
